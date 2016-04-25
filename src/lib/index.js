5816b29b9ce1e34750f4f28924d8d70d9737eed8
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-25 01:36:09 -0700

    bump

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 import { assert } from 'chai'
 
+const IS_DEV = process.env.NODE_ENV !== 'production'
+
 /**
  * Takes in an ordered mapping of names to stores and reduces to a redux store compatible interface that can dispatch and getState to all stores or specific ones.
  * @example <caption>Creates a store multiplexer that can dispatch and getState on all stores at once.</caption>
@@ -13,9 +15,9 @@ import { assert } from 'chai'
  * @return {Object}              An object that can dispatch and getState to all stores or each individually with some useful helpers.
  */
 export const createStoreMultiplexer = storeMapping => {
-  assert.ok(storeMapping, 'storeMapping is required')
-  assert(Array.isArray(storeMapping), 'storeMapping must be an array')
-  assert(storeMapping.every(x => Array.isArray(x) && x.length === 2), 'storeMapping must be an array of [<name>, <store>] arrays')
+  if(IS_DEV) assert.ok(storeMapping, 'storeMapping is required')
+  if(IS_DEV) assert(Array.isArray(storeMapping), 'storeMapping must be an array')
+  if(IS_DEV) assert(storeMapping.every(x => Array.isArray(x) && x.length === 2), 'storeMapping must be an array of [<name>, <store>] arrays')
 
   const storeMap = new Map(storeMapping)
   const mapReduceStores = operation => {
@@ -56,28 +58,40 @@ export const createStoreMultiplexer = storeMapping => {
  * @param  {...String} selectKeys The selection path to use with getState
  * @return {Object}               A sub store implementing redux store interface
  */
-export const bisectStore = (...selectKeys) => store => {
-  assert.ok(store, 'store must exist')
-  assert.ok(store.dispatch, 'store must define dispatch')
-  assert.ok(store.getState, 'store must define getState')
-  assert(selectKeys.length > 0, 'must define one or more keys to select on')
-
+export const bisectStore = (...selectKeys) => (store, defaultState) => {
+  if(IS_DEV) assert.ok(store, 'store must exist')
+  if(IS_DEV) assert.ok(store.dispatch, 'store must define dispatch')
+  if(IS_DEV) assert.ok(store.getState, 'store must define getState')
+  if(IS_DEV) assert(selectKeys.length > 0, 'must define one or more keys to select on')
+  const selectState = createStateSelector(...selectKeys)
   return  { dispatch: action => store.dispatch(action)
           , subscribe: listener => store.subscribe(listener)
-          , getState: () => selectState(selectKeys, store.getState())
+          , getState: () => selectState(store.getState(), defaultState)
           }
 }
 
-/** Selects a sub state from a state tree by path. */
-export const selectState = (selectKeys, state, defaultValue) => {
-  assert(Array.isArray(selectKeys), 'selectKeys must be an array.')
-  assert(selectKeys.length > 0, 'must specify a selection path')
-  assert.ok(state, 'state is required')
+
+/** Creates a function that selects a sub state from a state tree by path. */
+export const createStateSelector = (...selectKeys) => (state, defaultState) => {
+  const hasDefault = typeof defaultState !== 'undefined'
+  if(IS_DEV) assert(Array.isArray(selectKeys), 'selectKeys must be an array.')
+  if(IS_DEV) assert(selectKeys.length > 0, 'must specify a selection path')
+  if(IS_DEV) assert.ok(state, 'state is required')
   let result = state
   for(let selectKey of selectKeys) {
-    result = state[selectKey]
+    result = result[selectKey]
+    if(IS_DEV) assert.ok(hasDefault || result, `'${selectKey}' state must exist in redux state in key chain [${selectKeys.join(', ')}] (did you forget to import '${selectKeys[0]}' reducer from its library into your combined reducers?) ${JSON.stringify({ state })}`)
     if(!result)
       break
   }
-  return result || defaultValue
+  return result || defaultState
+}
+
+/**
+ * Creates a function that accepts state and ID for components that require state normalization across multiple instances and returns a function that will select state for the component.
+ * @param  {string[]|number[]} selectKeys 1 or more key arguments to select the root state to bisect on.
+ * @return {Function}                     Function that takes an ID and reutrns a normalized state for that ID.
+ */
+export const createStateBisector = (...selectKeys) => {
+  return id => createStateSelector(...selectKeys, id)
 }
